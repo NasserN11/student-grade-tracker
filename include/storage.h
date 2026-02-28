13601aa98d849d5139016616086b24366b199efd
Nasser <nasser.mahamat.swe@gmail.com>
2026-02-28 18:27:52 +0300

    Add saveCourses() function - Implement CSV file saving for courses - Each course saved as: name,credit,grade

--- include/storage.h
+++ include/storage.h
@@ -1,4 +1,15 @@
 #ifndef STUDENTGRADETRACKER_STORAGE_H
 #define STUDENTGRADETRACKER_STORAGE_H
 
+#include <string>
+#include <vector>
+#include "../include/calculation.h"
+
+using namespace std;
+
+string getDefaultFileName();
+
+bool saveCourses(const vector<Course> &courses, const string& filename);
+
+
 #endif //STUDENTGRADETRACKER_STORAGE_H
--- src/storage.cpp
+++ src/storage.cpp
@@ -1,3 +1,34 @@
-//
-// Created by nasse on 2/4/2026.
-//
+#include <string>
+#include <fstream>
+#include "../include/storage.h"
+
+using namespace std;
+
+string getDefaultFileName() {
+    return "courses.csv";
+}
+
+bool saveCourses(const vector<Course> &courses, const string& filename) {
+
+    // Create output file stream
+    ofstream file;
+    file.open(filename);
+
+
+    if (!file.is_open())
+        return false;
+
+    // Write each course
+    for (const auto& course : courses) {
+        file << course.courseName << ","
+             << course.courseCredit << ","
+             << course.courseGrade << "\n";
+    }
+
+    if (!file) {
+        file.close();
+        return false;
+    }
+
+    return true;
+}
